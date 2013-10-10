a1f6338e24dc593ee88ae8529355bd6dae525c16
paulomunoz <pvanet@gmail.com>
2013-10-10 11:07:21 +0100

    Update picturefill.js
    Adding background-image behaviour (will be improved)

--- picturefill.js
+++ picturefill.js
@@ -7,7 +7,29 @@
 
 	w.picturefill = function() {
 		var ps = w.document.getElementsByTagName( "span" );
-
+		var bgs = w.document.getElementsByClassName( "responsive-bg" );
+		
+		
+		for( var k = 0, kl = bgs.length; k < kl; k++ ){
+			var sources = bgs[ k ].getElementsByTagName( "span" ),
+			matches = [];
+			
+			// See if which sources match
+			for( var l = 0, ll = sources.length; l < ll; l++ ){
+				var media = sources[ l ].getAttribute( "data-media" );
+				// if there's no media specified, OR w.matchMedia is supported 
+				if( !media || ( w.matchMedia && w.matchMedia( media ).matches ) ){
+					matches.push( sources[ l ] );
+				}
+			}
+			
+			if( matches.length ){
+				var matchedEl = matches.pop();
+				bgs[ k ].style.backgroundImage =  "url('" + matchedEl.getAttribute( "data-src" ) + "')";
+			}
+			
+		}
+		
 		// Loop the pictures
 		for( var i = 0, il = ps.length; i < il; i++ ){
 			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
@@ -23,24 +45,24 @@
 						matches.push( sources[ j ] );
 					}
 				}
+	
+				// Find any existing img element in the picture element
+				var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
-			// Find any existing img element in the picture element
-			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
-
-			if( matches.length ){
-				var matchedEl = matches.pop();
-				if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
-					picImg = w.document.createElement( "img" );
-					picImg.alt = ps[ i ].getAttribute( "data-alt" );
+				if( matches.length ){
+					var matchedEl = matches.pop();
+					if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
+						picImg = w.document.createElement( "img" );
+						picImg.alt = ps[ i ].getAttribute( "data-alt" );
+					}
+	
+					picImg.src =  matchedEl.getAttribute( "data-src" );
+					matchedEl.appendChild( picImg );
 				}
-
-				picImg.src =  matchedEl.getAttribute( "data-src" );
-				matchedEl.appendChild( picImg );
+				else if( picImg ){
+					picImg.parentNode.removeChild( picImg );
+				}
 			}
-			else if( picImg ){
-				picImg.parentNode.removeChild( picImg );
-			}
-		}
 		}
 	};
 
